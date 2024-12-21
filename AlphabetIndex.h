47d9e32b6e0fc33cd692ec2e59b233e91717d0da
Vitaliy Koldashev <vita-rzn@yandex.ru>
2024-12-21 11:50:38 +0300

    added class Alphabeticaindex

--- AlphabetIndex.h
+++ AlphabetIndex.h
@@ -4,7 +4,6 @@
 #include <iostream>
 #include <fstream>
 #include <sstream>
-#include <vector>
 #include <string>
 #include <cctype>
 #include <algorithm>
@@ -13,12 +12,73 @@
 #include "Pages.h"
 #include "Rows.h"
 
-Dictionary<std::string, std::vector<int>> buildAlphabeticalIndexFromFile(
+class AlphaBeticalIndex
+{
+private:
+    Dictionary<std::string, DynamicArray<int>> index;
+public:
+    AlphaBeticalIndex() = default;
+    
+    AlphaBeticalIndex(Book& input_book)
+    {
+        for (int NumPage = 0; NumPage < input_book.GetPageCount(); NumPage++)
+        {
+            Page page = input_book.GetPage(NumPage);
+            for (int NumRow = 0; NumRow < page.getPageSize(); NumRow++)
+            {
+                Row row = page.getRow(NumRow);
+                std::istringstream rowStream(row.get());
+                std::string word;
+                while(rowStream >> word)
+                {
+                    if(index.contains(word))
+                    {
+                        DynamicArray<int> &pages = index.get(word);
+                        pages.push_back(NumPage + 1);
+                    }
+                    else
+                    {
+                        DynamicArray<int> buf(1, NumPage + 1);
+                        index.add(word, buf);
+                    }
+                }
+            }
+            
+        }
+    }
+    
+    Dictionary<std::string, DynamicArray<int>> GetIndex()
+    {
+        return index;
+    }
+    
+    void DictionaryToFile(const std::string& outputFilePath)
+    {
+        std::ofstream outputFile(outputFilePath);
+        if (!outputFile) {
+            throw std::runtime_error("Не удалось открыть выходной файл.");
+        }
+
+        for (auto it = index.begin(); it != index.end(); ++it) {
+            const std::pair<std::string, DynamicArray<int>> &entry = *it;
+            outputFile << entry.first << ": ";
+            for (size_t i = 0; i < entry.second.get_size(); ++i) {
+                outputFile << entry.second[i];
+                if (i < entry.second.get_size() - 1) {
+                    outputFile << ", ";
+                }
+            }
+            outputFile << std::endl;
+        }
+        outputFile.close();
+    }
+};
+
+
+AlphaBeticalIndex buildAlphabeticalIndexFromFile(
     const std::string &inputFilePath,
     int pageSize,
     const std::string &outputFilePath, int rowSize) {
-    
-    Dictionary<std::string, std::vector<int>> index;
 
     // Чтение текста из входного файла
     std::ifstream inputFile(inputFilePath);
@@ -47,6 +107,7 @@ Dictionary<std::string, std::vector<int>> buildAlphabeticalIndexFromFile(
     DynamicArray<Page> pages = PageMaker(rows, pageSize);
     
     Book item(pages);
+    
     std::string bookOutputFilePath = "/Users/vitalijkoldasev/Desktop/laboratories_3_sem/laboratory3/laboratory3/outputbook.txt";
     std::ofstream bookOutputFile(bookOutputFilePath);
     if (!bookOutputFile) {
@@ -66,60 +127,12 @@ Dictionary<std::string, std::vector<int>> buildAlphabeticalIndexFromFile(
     }
 
     bookOutputFile.close();
-
-    std::istringstream wordsStream(inputText);
-    std::vector<std::string> words;
-    std::string word;
-    while (wordsStream >> word) {
-        words.push_back(word);
-    }
-
-
-    for (int NumPage = 0; NumPage < item.GetPageCount(); NumPage++)
-    {
-        Page page = item.GetPage(NumPage);
-        for (int NumRow = 0; NumRow < page.getPageSize(); NumRow++)
-        {
-            Row row = page.getRow(NumRow);
-            std::istringstream rowStream(row.get());
-            std::string word;
-            while(rowStream >> word)
-            {
-                if(index.contains(word))
-                {
-                    std::vector<int> &pages = index.get(word);
-                    if (pages.empty() || pages.back() != NumPage)
-                    {
-                        pages.push_back(NumPage + 1);
-                    }
-                }
-                else
-                {
-                    index.add(word, {NumPage + 1});
-                }
-            }
-        }
-        
-    }
     
-    std::ofstream outputFile(outputFilePath);
-    if (!outputFile) {
-        throw std::runtime_error("Не удалось открыть выходной файл.");
-    }
+    AlphaBeticalIndex index(item);
+    
 
-    for (auto it = index.begin(); it != index.end(); ++it) {
-        const std::pair<std::string, std::vector<int>> &entry = *it;
-        outputFile << entry.first << ": ";
-        for (size_t i = 0; i < entry.second.size(); ++i) {
-            outputFile << entry.second[i];
-            if (i < entry.second.size() - 1) {
-                outputFile << ", ";
-            }
-        }
-        outputFile << std::endl;
-    }
-
-    outputFile.close();
+    index.DictionaryToFile(outputFilePath);
+    
     return index;
 }
 
